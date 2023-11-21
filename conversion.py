3e760b2436293ff76a972e8e999a0ea023229f6a
Kacper Bazan <bazankacper25@gmai.com>
2023-11-20 23:19:01 -0500

    corrected Cd graph

---
+++ conversion.py
@@ -0,0 +1,20 @@
+# A file that has constants to allow for easy conversions
+
+lbm_to_kg = 0.45359237
+kg_to_lbm = 1/lbm_to_kg
+
+in_to_m = 0.0254
+m_to_in = 1/in_to_m
+
+ft_to_m = 0.3048
+m_to_ft = 1/ft_to_m
+
+psi_to_pa = 6894.757
+pa_to_psi = 1/psi_to_pa
+
+ft2_to_m2 = 0.09290304
+m2_to_ft2 = 1/ft2_to_m2
+
+in2_to_m2 = in_to_m**2
+m2_to_in2 = 1/in2_to_m2
+
